ee99d7c7d5993c392268cbc0e8c6501903688076
Jason Livesay <ithkuil@gmail.com>
2015-04-24 10:04:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function addScriptsrc) {
+  var proj = document.getElementById('run').value;
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://raw.githubusercontent.com/'+proj+'/master/index.js?zz='+(new Date()).getTime();
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
 
 scriptBase = 'https://raw.githubusercontent.com/runvnc/holoships/master/';
 addScript('three.min.js');
