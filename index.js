c5e3301743f6bfdb8a0b73ccdfe889b022feaaf9
Jason Livesay <ithkuil@gmail.com>
2015-04-24 11:22:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ function addScript(base, src) {
 }
 
 function addScript2(base, src) {
-  alert('addscript2');
+  
 	  (function() {
     var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
     po.src = src;
     var s = document.getElementsByTagName('head')[0]; s.appendChild(po);
-    alert('done');
+ 
   })();	
 }
 
 var base = 'https://raw.githubusercontent.com/mrdoob/three.js/master/';
-addScript(base, 'build/three.min.js');
-addScript(base, 'examples/js/renderers/Projector.js');
-addScript(base, 'examples/js/renderers/CanvasRenderer.js');
-addScript('https://raw.githubusercontent.com/runvnc/holoships/master/', 'game.js');
+addScript2(base, 'build/three.min.js');
+addScript2(base, 'examples/js/renderers/Projector.js');
+addScript2(base, 'examples/js/renderers/CanvasRenderer.js');
+addScript2('https://raw.githubusercontent.com/runvnc/holoships/master/', 'game.js');
 
 
 
