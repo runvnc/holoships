41546ea2969fbdcfaf8c7c428856042a9adc1721
Jason Livesay <ithkuil@gmail.com>
2015-04-30 03:43:36 +0000

    update

--- main.js
+++ main.js
@@ -12,9 +12,10 @@ canv.style.border = '1px solid red';
 document.body.appendChild(canv);
 
 var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
-                       window.mozRequestAnimationFrame || window.msRequestAnimationFrame || 
-                       function(c) {window.setTimeout(c, 15)};
-var requestAnimFrame = requestAnimationFrame;
+                       window.mozRequestAnimationFrame || window.msRequestAnimationFrame ||
+
+var requestAnimFrame = function(c) {window.setTimeout(c, 15)};
+//var requestAnimFrame = requestAnimationFrame;
 /**
    Phoria
    pho·ri·a (fôr-)
